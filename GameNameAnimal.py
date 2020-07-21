d94b20ef59dcf030bdfc002c75f1ec7f4284b3bd
Sujata018 <60694602+Sujata018@users.noreply.github.com>
2020-07-21 10:52:36 +0530

    Add files via upload

--- GameNameAnimal.py
+++ GameNameAnimal.py
@@ -14,157 +14,11 @@
     
 '''
 
-# Database for animals
-
-animals = { 'a':{"aardwolf":0,"aardvark":0,"albatross":0,"alpaca":0,"anemone":0,"anteater":0,"antelope":0,"ape":0,"armadillo":0,"axolotl":0,"aye-aye":0},
-            'b':{"bandicoot":0,"barnacle":0,"barracuda":0,"bat":0,"bass":0,"bear":0,"beaver":0,"bison":0,"boar":0,"bobcat":0,"bullfrog":0},
-            'c':{"caiman":0,"camel":0,"cpybara":0,"caracal":0,"cassowary":0,"cat":0,"cheetah":0,"chimpanzee":0,"chinchilla":0,"chipmunk":0,"coati":0,"cougar":0,"cow":0,"coyote":0},
-            'd':{"deer":0,"dingo":0,"dog":0,"donkey":0,"dugong":0,"dolphin":0},
-            'e':{"eland":0,"elephant":0,"elk":0},
-            'f':{"ferret":0,"finch":0,"fossa":0,"fox":0,"frog":0},
-            'g':{"gazelle":0,"goat":0,"groundhog":0,"guineapig":0},
-            'h':{"hedgehog":0,"hippopotamus":0,"horse":0,"hyena":0},
-            'i':{},
-            'j':{},
-            'k':{"koala":0},
-            'l':{"leopard":0,"lion":0,"llama":0,"lynx":0},
-            'm':{"meerkat":0,"mole":0,"moose":0,"mouse":0},
-            'n':{},
-            'o':{"orangutan":0,"orca":0},
-            'p':{"panda":0,"panther":0,"pig":0,"polar-bear":0},
-            'q':{},
-            'r':{"rabbit":0,"raccoon":0,"reindeer":0,"robin":0,"rat":0},
-            's':{"sea-lion":0,"seagull":0,"seahorse":0,"seal":0,"sea-otter":0,"sheep":0,"slug":0,"squirrel":0},
-            't':{"tiger":0,"turtle":0, "tortoise":0},
-            'u':{},
-            'v':{},
-            'w':{"wallaby":0,"walrus":0,"wasp":0,"weasel":0,"weaver":0,"whale":0,"wildcat":0,"wilddog":0,"wolf":0,"wolverine":0,"wombat":0},
-            'x':{},
-            'y':{},
-            'z':{}
-           }
-
-# Database for birds
-
-birds = { 'a':{"albatross":0},
-            'b':{},
-            'c':{"crane":0,"chicken":0},
-            'd':{"dodo":0,"dove":0,"duck":0},
-            'e':{"eagle":0,"echidnas":0,"emu":0},
-            'f':{"flamingo":0, "falcon":0},
-            'g':{},
-            'h':{},
-            'i':{},
-            'j':{},
-            'k':{},
-            'l':{},
-            'm':{},
-            'n':{},
-            'o':{},
-            'p':{},
-            'q':{},
-            'r':{},
-            's':{},
-            't':{},
-            'u':{},
-            'v':{},
-            'w':{},
-            'x':{},
-            'y':{},
-            'z':{}
-           }
-
-
-# Database for insects
-
-insects = {'a':{"ant":0},
-            'b':{"beetle":0,"bee":0,"butterfly":0},
-            'c':{"cockroach":0,"caterpillar":0, "centipede":0},
-            'd':{"dragonfly":0},
-            'e':{"earwig":0},
-            'f':{"firefly":0, "flea":0,"fly":0},
-            'g':{},
-            'h':{},
-            'i':{},
-            'j':{},
-            'k':{},
-            'l':{},
-            'm':{},
-            'n':{},
-            'o':{},
-            'p':{},
-            'q':{},
-            'r':{},
-            's':{},
-            't':{},
-            'u':{},
-            'v':{},
-            'w':{},
-            'x':{},
-            'y':{},
-            'z':{}
-           }
-
-# Database for fishes
-
-fishes = {  'a':{"angelfish":0},
-            'b':{},
-            'c':{"cuttlefish":0,"catfish":0,"clown":0},
-            'd':{},
-            'e':{"eel":0},
-            'f':{"fish":0,"flounder":0},
-            'g':{},
-            'h':{},
-            'i':{},
-            'j':{},
-            'k':{},
-            'l':{},
-            'm':{},
-            'n':{},
-            'o':{},
-            'p':{},
-            'q':{},
-            'r':{},
-            's':{},
-            't':{},
-            'u':{},
-            'v':{},
-            'w':{},
-            'x':{},
-            'y':{},
-            'z':{}
-           }
-
-
-# Database for reptiles
-
-reptiles ={ 'a':{"alligator":0,"anaconda":0},
-            'b':{"boa":0},
-            'c':{"crocodile":0,"cobra":0,"chameleon":0},
-            'd':{},
-            'e':{},
-            'f':{},
-            'g':{},
-            'h':{},
-            'i':{},
-            'j':{},
-            'k':{},
-            'l':{},
-            'm':{},
-            'n':{},
-            'o':{},
-            'p':{},
-            'q':{},
-            'r':{},
-            's':{},
-            't':{},
-            'u':{},
-            'v':{},
-            'w':{},
-            'x':{},
-            'y':{},
-            'z':{}
-    }
+from DictionariesAnimals import animals
+from DictionariesAnimals import birds
+from DictionariesAnimals import insects
+from DictionariesAnimals import fishes
+from DictionariesAnimals import reptiles
 
 def getAnimal(ch):  # This function gets a new animal name from the animal directory, starting the the letter passed as parameter.
 
@@ -224,8 +78,9 @@ if __name__ == '__main__':
     last_letter = ""
     while gameOn:
         if firstInstruction:
-            print("You type an animal name, I type another starting with your last letter, then you type another.")
-            print("Who gives up first, looses. Let's start!!")
+            print("You type a name of an animal, I type another starting with your last letter, then you type another.")
+            print("Who gives up first, looses.")
+            print("Let's start!!")
             uAnimal = input("Enter name of an animal:")
             firstInstruction = False
             i,p = validateAnimal(uAnimal)
